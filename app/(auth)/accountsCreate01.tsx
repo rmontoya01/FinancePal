f342b37ddf837d9f4fbef19743525a0099951331
rmontoya01 <rmontoya01@aurora.edu>
2025-02-19 10:08:33 -0600

    App Icon added and edits to create accounts page.
    Added three image icons to the app.json file and then made changes to the create accounts screen with more text infields.

--- app/(auth)/accountsCreate01.tsx
+++ app/(auth)/accountsCreate01.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useRef } from 'react'
 
 import ScreenWrapper from '@/components/ScreenWrapper';
 import PreviousButton from '@/components/PreviousButton';
@@ -10,13 +10,79 @@ import Input from '@/components/Input';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { verticalScale } from '@/utils/styling';
 import Button from "@/components/Button";
+import { useRouter } from 'expo-router';
 
 const accountsCreate01 = () => {
+
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+    const nameRef = useRef("");
+    // const [isLoading, setIsLoading] = useState(false);
+
+    const router = useRouter();
+
+    const onSubmit = async () => {
+        if (!emailRef.current || !passwordRef.current || !nameRef.current) {
+            alert("Please fill in all of fields");
+            return;
+        }
+        console.log("email: ", emailRef.current);
+        console.log("password: ", passwordRef.current);
+        console.log("name: ", nameRef.current);
+        console.log("submitting... it is valid");
+    }
+
     return (
-        <ScreenWrapper style={styles.container}>
-            <PreviousButton iconSize={30} />
-            <View style={{ gap: 10, marginTop: spacingY._20, alignItems: 'center' }}>
-                <Typo>Create your account here</Typo>
+        <ScreenWrapper>
+            <View style={styles.container}>
+                <PreviousButton iconSize={30} />
+
+                <View style={{ gap: 10, marginTop: spacingY._20, alignItems: 'center' }}>
+                    <Typo>Create your account here</Typo>
+                </View>
+
+                {/* create fields */}
+                <View style={{ paddingHorizontal: spacingX._20, gap: spacingY._20, marginTop: spacingY._20 }}>
+                    <Typo>
+                        Create your account here!
+                    </Typo>
+                    <Input
+                        placeholder="Email"
+                        onChangeText={(text) => emailRef.current = text}
+                    />
+                </View>
+
+                {/* Password */}
+                <View style={{ paddingHorizontal: spacingX._20, gap: spacingY._20, marginTop: spacingY._20 }}>
+                    <Input
+                        placeholder="Password"
+                        onChangeText={(text) => emailRef.current = text}
+                        secureTextEntry={true}
+                    />
+                </View>
+
+                <View style={{ paddingHorizontal: spacingX._20, gap: spacingY._20, marginTop: spacingY._20 }}>
+                    <Input
+                        placeholder="Confirm Password"
+                        onChangeText={(text) => emailRef.current = text}
+                        secureTextEntry={true}
+                    />
+                </View>
+
+                <View style={{ paddingHorizontal: spacingX._20, gap: spacingY._20, marginTop: spacingY._20 }}>
+                    <Input
+                        placeholder="Full Name"
+                        onChangeText={(text) => emailRef.current = text}
+                    />
+                </View>
+
+                <View style={{ paddingHorizontal: spacingX._20, gap: spacingY._20, marginTop: spacingY._20 }}>
+                    <Input
+                        placeholder="Fill In Blank"
+                        onChangeText={(text) => emailRef.current = text}
+                    />
+                </View>
+
             </View>
         </ScreenWrapper>
     )
