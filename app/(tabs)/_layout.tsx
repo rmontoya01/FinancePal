e17d6cdded231d6d0c98054a3635694f57761e82
rmontoya01 <rmontoya01@aurora.edu>
2025-02-23 00:28:07 -0600

    WIP tab bar customization
    Work in progress for tab bar navigation customization. Created a new component called TabBarCuztomise.tsx under the components directory.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,10 +2,16 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 
+import { TabBarCustomize } from '@/components/TabBarCustomize'
+
 const _layout = () => {
   return (
-    <Tabs>
-
+    <Tabs tabBar={TabBarCustomize} screenOptions={{ headerShown: true }}>
+      <Tabs.Screen name="budget" />
+      <Tabs.Screen name="history" />
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="stats" />
+      <Tabs.Screen name="converter" />
     </Tabs>
   )
 }
---
+++ components/TabBarCustomize.tsx
@@ -0,0 +1,59 @@
+import { View, Platform, TouchableOpacity } from 'react-native';
+import { useLinkBuilder, useTheme } from '@react-navigation/native';
+import { Text, PlatformPressable } from '@react-navigation/elements';
+import { BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { colors } from '@/constants/themes';
+
+export function TabBarCustomize({ state, descriptors, navigation }: BottomTabBarProps) {
+
+    return (
+        <View style={{ flexDirection: 'row', width: "100%", height: 100 }}>
+            {state.routes.map((route, index) => {
+                const { options } = descriptors[route.key];
+                const label: any =
+                    options.tabBarLabel !== undefined
+                        ? options.tabBarLabel
+                        : options.title !== undefined
+                            ? options.title
+                            : route.name;
+
+                const isFocused = state.index === index;
+
+                const onPress = () => {
+                    const event = navigation.emit({
+                        type: 'tabPress',
+                        target: route.key,
+                        canPreventDefault: true,
+                    });
+
+                    if (!isFocused && !event.defaultPrevented) {
+                        navigation.navigate(route.name, route.params);
+                    }
+                };
+
+                const onLongPress = () => {
+                    navigation.emit({
+                        type: 'tabLongPress',
+                        target: route.key,
+                    });
+                };
+
+                return (
+                    <TouchableOpacity
+                        // href={buildHref(route.name, route.params)}
+                        accessibilityState={isFocused ? { selected: true } : {}}
+                        accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarButtonTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        style={{ flex: 1 }}
+                    >
+                        <Text style={{ color: isFocused ? colors.primary : colors.text }}>
+                            {label}
+                        </Text>
+                    </TouchableOpacity>
+                );
+            })}
+        </View>
+    );
+}
