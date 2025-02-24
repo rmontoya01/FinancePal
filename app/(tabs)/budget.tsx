50467645b5d4150aeb8186c071e4761836191abb
EduardoM236 <eduardo.gordom123@gmail.com>
2025-02-24 10:24:05 -0600

    Making the UI for both history and budget from powerpoint of sprint 1
    Making the UI look like the demo for sprint review 1

--- app/(tabs)/budget.tsx
+++ app/(tabs)/budget.tsx
@@ -1,15 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
 
 const budget = () => {
     return (
         <ScreenWrapper>
-            <Text>budget</Text>
+            <Text style={styles.text}>Create a budget</Text>
+
+            <TouchableOpacity style={styles.container}>
+                <Text>Income bro</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.container}>
+                <Text>Spendings</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.container}>
+                <Text>add to Budget</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.container}>
+                <Text>Finish Budget</Text>
+            </TouchableOpacity>
+
         </ScreenWrapper>
     )
 }
 
 export default budget
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        margin: 30,
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: 'lightblue',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#fff',
+    },
+    containerText: {
+        justifyContent: 'center',
+
+        alignItems: 'center',
+        backgroundColor: 'lightblue',
+        padding: 50,
+        height: 10,
+        width: 50,
+
+        borderRadius: 20,
+    },
+})
--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -16,9 +16,7 @@ export default function History() {
     <View style={styles.container}>
       <Text style={styles.text}>History Screen Page</Text>
       <View style={styles.containerText}>
-        <div style={styles.borderText}>
-          <Text style={styles.text}>History</Text>
-        </div>
+
         {/* <FlashList
           data={DATA}
           renderItem={({ item }) => <Text style={styles.listText}>{item.title}</Text>}
@@ -44,24 +42,15 @@ const styles = StyleSheet.create({
   containerText: {
     backgroundColor: 'lightblue',
     padding: 50,
-    height: 50,
-    width: 50,
+    height: 500,
+    width: 250,
     marginBlockStart: "10%",
+    borderRadius: 20,
   },
   listText: {
     backgroundColor: "red",
     fontSize: 20,
     fontStyle: 'italic',
   },
-  borderText: {
-    borderWidth: 1,
-    borderColor: 'white',
-    borderStyle: 'solid',
-    borderRadius: 10,
-    padding: 10,
-    margin: 10,
-    color: 'white',
-    fontSize: 20,
-    fontStyle: 'italic',
-  }
+
 });
