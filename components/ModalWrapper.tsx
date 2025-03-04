2fcdc5e09b789c5cd303eae58f057639df60d342
rmontoya01 <rmontoya01@aurora.edu>
2025-03-04 09:38:29 -0600

    Updated ModalWrapper component.tsx
    Made changes to the modal wrapper component.

--- components/ModalWrapper.tsx
+++ components/ModalWrapper.tsx
@@ -1,26 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { colors } from '@/constants/themes'
+import { colors, spacingY } from '@/constants/themes'
 import { ModalWrapperProps } from '@/types'
 import Typo from './Typo'
 import Ionicons from '@expo/vector-icons/Ionicons'
+
+
+const isIos = Platform.OS === 'ios';
 
 const ModalWrapper = ({
     style,
     children,
     background = colors.neutral900
 }: ModalWrapperProps) => {
+
     return (
-        <View>
-            <Text>ModalWrapper</Text>
-            <View style={styles.container}>
-                <Typo>IncomeModal</Typo>
-                {/* <StatusBar style="auto" /> */}
-                <Ionicons name="cash-sharp" size={24} color="black" />
-            </View>
+        <View style={[styles.container, { backgroundColor: background }, style && style]}>
+            {children}
         </View>
-    )
-}
+    );
+};
 
 export default ModalWrapper
 
@@ -28,5 +27,7 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.neutral900,
         flex: 1,
+        paddingTop: isIos ? spacingY._17 : 40,
+        paddingBottom: isIos ? spacingY._17 : spacingY._12
     },
 })
